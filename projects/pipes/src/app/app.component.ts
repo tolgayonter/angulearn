c731d456757b10d01e6423ab05962956cd3a0d0d
tolgayonter <tolgayonter@gmail.com>
2023-03-02 21:59:33 +0300

    add `pipes` app

---
+++ projects/pipes/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string | undefined
+  date: string | undefined
+  amount: number | undefined
+
+  onNameChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value
+    this.name = value
+  }
+
+  onDateChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value
+    this.date = value
+  }
+
+  onAmountChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value
+    this.amount = parseFloat(value)
+  }
+}
