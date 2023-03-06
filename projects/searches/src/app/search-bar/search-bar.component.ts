9160fc031268f6c14acd72b909188e219b979732
tolgayonter <tolgayonter@gmail.com>
2023-03-06 18:40:51 +0300

    add `searches` app

---
+++ projects/searches/src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,23 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent {
+
+  @Output() submitted = new EventEmitter<string>()
+  term = ''
+
+  onInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value
+    this.term = value
+  }
+
+  onSubmit(event: Event) {
+    event.preventDefault()
+    this.submitted.emit(this.term)
+  }
+
+}
