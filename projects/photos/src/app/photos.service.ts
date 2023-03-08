f3a7ba6b5a106fcc4a930c7b539ea6de66716ef2
tolgayonter <tolgayonter@gmail.com>
2023-03-08 18:25:22 +0300

    add `photos` app

---
+++ projects/photos/src/app/photos.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface UnsplashResponse {
+  urls: {
+    regular: string
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotosService {
+
+  constructor(private http: HttpClient) { }
+
+  getPhoto() {
+    return this.http.get<UnsplashResponse>('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 'Client-ID saRZmSGvNga9JJf-98qIJTUmZ-nctk7IX4ExWkCsXwQ'
+      }
+    })
+  }
+
+}
